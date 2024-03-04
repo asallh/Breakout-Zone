1dbfa1013a2fb681e68b764b2f56462a89988a1c
asallh <ajaypalsallh@gmail.com>
2024-03-03 21:20:31 -0700

    setting the pre-dev launch page
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import styled from "styled-components";
 
 export default function App() {
+  const title = "Welcome to Breakout Zone";
   return (
     <>
       <div className="app">
         <div className="content">
-          <h1>App Component</h1>
+          <h1 className="heading-title">{title}</h1>
+          <h2 className="sub-heading">See you soon.</h2>
         </div>
       </div>
     </>
