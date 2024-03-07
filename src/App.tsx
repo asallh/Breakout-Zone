abefaf9888f6aaad9f7a80ebc47dc21583f4c5db
asallh <ajaypalsallh@gmail.com>
2024-03-06 23:52:43 -0700

    Set up Shad UI for the components
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import "./index.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/HomeContent";
+import { Switch } from "./components/ui/switch";
 
 export default function App() {
   const title = "Welcome to Breakout Zone";
   return (
-    <>
-      <div className="app">
-        <div className="content">
-          <h1 className="heading-title">{title}</h1>
-          <NavBar />
-          <h2 className="sub-heading">See you soon.</h2>
-          <Home />
-        </div>
+    <div className="app">
+      <div className="content">
+        <h1 className="heading-title">{title}</h1>
+        <NavBar />
+        <h2 className="sub-heading">See you soon.</h2>
+        <Home />
       </div>
-    </>
+    </div>
   );
 }
