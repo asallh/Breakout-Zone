a0d239930e6aeb91e338e7e8fef1d9c35ed1ade1
asallh <ajaypalsallh@gmail.com>
2024-03-09 21:34:35 -0700

    Small Changes
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,6 @@ export default function NavBar() {
     <div className="w-screen px-8">
       <div
         className="flex justify-between w-full p-4 px-8 border border-white rounded-xl"
-        // style={{ width: "100vw" }}
       >
         {/* Replace this with image when i get that chance to make a logo */}
         <h1 className="flex items-center justify-start text-small font-bauer">
@@ -22,10 +21,15 @@ export default function NavBar() {
           <DropdownMenu>
             <DropdownMenuTrigger>Select Versus</DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuLabel>Players</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem>1 v 1</DropdownMenuItem>
+              <DropdownMenuItem>Single Player</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuLabel>Team</DropdownMenuLabel>
+              <DropdownMenuSeparator />
               <DropdownMenuItem>Team v Team</DropdownMenuItem>
+              <DropdownMenuItem>Single Team</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
