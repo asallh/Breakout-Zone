73c84796eff858db5c213cef7ec007890415fa9a
asallh <ajaypalsallh@gmail.com>
2024-03-09 21:23:37 -0700

    Inital Nav Bar layout
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import NavBar from "./components/NavBar";
 export default function App() {
   const title = "Breakout Zone";
   return (
-    <div className="app">
-      <div className="">
-        {/* Team Selection Bar */}
-        <NavBar />
+    <div className="py-16">
+      {/* Team Selection Bar */}
+      <NavBar />
+      <div className="p-8">
         {/* Where the cards get rendered */}
         <Home />
       </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,25 +9,26 @@ import {
 
 export default function NavBar() {
   return (
-    
-    <div
-      className="flex justify-between p-4 px-8 border border-white rounded-xl"
-      style={{ width: "100vw" }}
-    >
-      {/* Replace this with image when i get that chance to make a logo */}
-      <h1 className="flex items-center justify-start text-small font-bauer">
-        Breakout Zone
-      </h1>
-      <div className="flex items-center">
-        <DropdownMenu>
-          <DropdownMenuTrigger>Select Versus</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>1 v 1</DropdownMenuItem>
-            <DropdownMenuItem>Team v Team</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <div className="w-screen px-8">
+      <div
+        className="flex justify-between w-full p-4 px-8 border border-white rounded-xl"
+        // style={{ width: "100vw" }}
+      >
+        {/* Replace this with image when i get that chance to make a logo */}
+        <h1 className="flex items-center justify-start text-small font-bauer">
+          Breakout Zone
+        </h1>
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Select Versus</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>1 v 1</DropdownMenuItem>
+              <DropdownMenuItem>Team v Team</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </div>
   );
